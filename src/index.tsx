598c82a9700fb1383d3011e533f9c3bac130717b
Drew Muliou <andreyjedai@tut.by>
2020-02-19 22:57:24 +0300

    Added Redux devtools

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import rootReducer from "./store";
-import App from "./components/App";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './store';
+import App from './components/App';
 
 //Создаём экземпляр стора на основании редьюсеров (так как там прописано как на какие экшены реагировать, инит стор и тд)
-const store = createStore(rootReducer);
-
+const store = createStore(
+	rootReducer,
+	(window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+		(window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-    //Оборачиваем приложение в редакс (из всего дом дерева будет доступен сейчас стор с плюшками)
-    <Provider store={store}>
-        <div className="App">
-            <App/>
-        </div>
-    </Provider>, document.getElementById('root'));
+	//Оборачиваем приложение в редакс (из всего дом дерева будет доступен сейчас стор с плюшками)
+	<Provider store={store}>
+		<div className='App'>
+			<App />
+		</div>
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
-import {combineReducers} from 'redux';
-import {reducer as todosReducer} from './todos/todos.reducers';
+import { combineReducers } from 'redux';
+import { reducer as todosReducer } from './todos/todos.reducers';
 
 //Задаём базовую структуру стора, объединяем (здесь можем объединять) все редьюсеры, которые есть в приложении
 export default combineReducers({
-    todos: todosReducer
+	todos: todosReducer,
 });
