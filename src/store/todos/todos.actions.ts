8ae27184c29dd82a5a257570843f6113f398af0b
Andrei Mulev <a.mulev.vironit@vironit.ru>
2020-02-28 22:29:39 +0300

    style fix

--- src/store/todos/todos.actions.ts
+++ src/store/todos/todos.actions.ts
@@ -1,12 +1,12 @@
-import {TodoInterface} from "../../interfaces/todo.interface";
-import {Action} from "../interfaces/action.interface";
+import { TodoInterface } from '../../interfaces/todo.interface';
+import { Action } from '../interfaces/action.interface';
 
 // Для удобства создаём список экшенов (именно их строковых типов)
 export const Actions = {
-    ADD_TODO: '[todo] Add todo',
-    UPDATE_TODO: '[todo] Update todo',
-    DELETE_TODO: '[todo] Delete todo',
-    TOGGLE_TODO: '[todo] Toggle todo',
+	ADD_TODO: '[todo] Add todo',
+	UPDATE_TODO: '[todo] Update todo',
+	DELETE_TODO: '[todo] Delete todo',
+	TOGGLE_TODO: '[todo] Toggle todo',
 };
 
 // Создаём генераторы экшенов - функции, которые будут возвращать объекты с полями type и payload
@@ -14,28 +14,30 @@ export const Actions = {
 // можно глянуть реализацию самого интерфейса
 // В пэйлоад бросаем тот тип данных, который указан у экшена в дженерике (повторю: реализация может быть разная, предлагаю к рассмотрению данную)
 export const addTodoAction = (text: string): Action<TodoInterface> => ({
-    type: Actions.ADD_TODO,
-    payload: {
-        id: (new Date()).getTime(),
-        text
-    },
-
+	type: Actions.ADD_TODO,
+	payload: {
+		id: new Date().getTime(),
+		text,
+	},
 });
 
-export const updateTodoAction = (id: number, text: string): Action<TodoInterface> => ({
-    type: Actions.ADD_TODO,
-    payload: {
-        id,
-        text
-    },
+export const updateTodoAction = (
+	id: number,
+	text: string
+): Action<TodoInterface> => ({
+	type: Actions.UPDATE_TODO,
+	payload: {
+		id,
+		text,
+	},
 });
 
 export const deleteTodoAction = (id: number): Action<number> => ({
-    type: Actions.ADD_TODO,
-    payload: id,
+	type: Actions.DELETE_TODO,
+	payload: id,
 });
 
 export const toggleTodoAction = (id: number): Action<number> => ({
-    type: Actions.TOGGLE_TODO,
-    payload: id,
+	type: Actions.TOGGLE_TODO,
+	payload: id,
 });
